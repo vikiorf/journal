3b4f4edd37ee71aecad8ee4a30b883b00d740b9d
vikiorf <45963051+vikiorf@users.noreply.github.com>
2020-12-10 18:39:16 +0100

    added apiKey to gitIgnore, changed API-appId

--- src/firebase/firebaseConf.js
+++ src/firebase/firebaseConf.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
   apiKey: apiKey,
-  authDomain: "health-a6022.firebaseapp.com",
-  databaseURL: "https://health-a6022.firebaseio.com",
-  projectId: "health-a6022",
-  storageBucket: "health-a6022.appspot.com",
-  messagingSenderId: "985473366452",
-  appId: "1:985473366452:web:800ebbe7875cc2fccbdae1"
-};
+  authDomain: 'health-a6022.firebaseapp.com',
+  databaseURL: 'https://health-a6022.firebaseio.com',
+  projectId: 'health-a6022',
+  storageBucket: 'health-a6022.appspot.com',
+  messagingSenderId: '985473366452',
+  appId: '1:985473366452:web:3c0af57e74f85a75cbdae1'
+}
 
 firebase.initializeApp(firebaseConfig)
 
