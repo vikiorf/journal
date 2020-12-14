ec5dede483b42b7d3c6b2aaf4579b8626b13bc7e
vikiorf <45963051+vikiorf@users.noreply.github.com>
2020-12-14 09:43:32 +0100

    started on sidebar in vue

--- vue/journal/src/App.vue
+++ vue/journal/src/App.vue
@@ -1,18 +1,20 @@
 <template>
-  <div id="nav">
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </div>
+  </div> -->
   <router-view />
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  display: grid;
+  grid-area: 1 / 1 / 13 / 13;
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-rows: repeat(12, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
 }
 
 #nav {
---
+++ vue/journal/src/components/SideBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="test">
+    <ul>
+      hej
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .test {
+    background-color: green;
+  }
+</style>
